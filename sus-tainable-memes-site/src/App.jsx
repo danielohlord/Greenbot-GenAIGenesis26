134096603617c6addeff4d2f11ffc67c9a612934
ly202009 <leeyayoh@gmail.com>
2026-03-14 13:59:23 -0400

    Frontend Added Input

--- sus-tainable-memes-site/src/App.jsx
+++ sus-tainable-memes-site/src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+import TextInput from './components/TextInput'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Sus-tainable Memes</h1>
+      <Navbar />
+      <Header />
+      <TextInput />
       <p>This is my react website.</p>
     </div>
   )
---
+++ sus-tainable-memes-site/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar(){
+    return(
+        <nav>
+            <h2>Sus-tainable Memes</h2>
+            <ul>
+                <li>Home</li>
+                <li>Memes</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
