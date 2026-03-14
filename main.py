a65fd3926f681b8bd42b0917a780f4a0b28342e1
Daniel <danieljm.lee@mail.utoronto.ca>
2026-03-14 11:51:21 -0400

    generate_image function added

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import os
 from google import genai
+from PIL import Image
+
 
 API_KEY = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=API_KEY)
 
-prompt = "Which model AI are you? Also, explain the quantum convergence theroem"
-response = client.models.generate_content(
-    model="gemini-3-flash-preview",
-    contents=prompt
-)
-print(response.text)
+prompt = input("Input your sustainability thing: ")
 
-print("Hello world")
+def amplify_input(prompt):  
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=prompt
+    )
+    return response.text
+
+def generate_image(prompt):
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-image-preview"
+        contents=prompt
+    )
+print(amplify_input(prompt))
